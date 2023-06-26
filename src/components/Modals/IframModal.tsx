41a4003ac5f077e58ad25d293f9d4cba443adb4b
Helvin Rymer <helvinrymer@helvins-mbp.myfiosgateway.com>
2023-06-25 22:27:59 -0400

    small updates

--- src/components/Modals/IframModal.tsx
+++ src/components/Modals/IframModal.tsx
@@ -19,7 +19,7 @@ const IframModal = () => {
 				src='https://app.acuityscheduling.com/schedule.php?owner=26143141'
 				title='Schedule Appointment'
 				width='100%'
-				height='100%'
+				height='800'
 			/>
 			{/* </div> */}
 		</div>

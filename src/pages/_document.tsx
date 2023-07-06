a114f1dfcf2b346d124b65ff7bc468145dc43241
Helvin Rymer <helvinrymer@helvins-mbp.myfiosgateway.com>
2023-07-05 21:08:50 -0400

    small updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,18 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head />
+				<Head>
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link
+						rel='preconnect'
+						href='https://fonts.gstatic.com'
+						cross-origin
+					/>
+					<link
+						href='https://fonts.googleapis.com/css2?family=GFS+Didot&family=Roboto:wght@400;700&display=swap'
+						rel='stylesheet'
+					/>
+				</Head>
 				<body>
 					<Main />
 					<Script
